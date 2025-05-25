23734cdfa74325cc11e54d74cb598be76f39bc6b
Bürgisser Reto rburgiss <reto.buergisser@stud.hslu.ch>
2025-05-25 18:40:03 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    // ①  CSS first – this lets any of your later global.scss overrides win
+    // import 'bootstrap/dist/css/bootstrap.min.css';
+    // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+</script>
+
+<nav class="navbar">
+    <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link">File</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">New</a></li>
+                <li><a class="dropdown-item" href="#">Save as ...</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link">Settings</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Preferences</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+<slot  />
+<footer class="footer">
+    <div class="container-fluid bg-light">
+        Ascii Diagrams Drawer
+    </div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+    import {invoke} from "@tauri-apps/api/core";
+
+    let name = $state("");
+    let greetMsg = $state("");
+
+    async function greet(event) {
+        event.preventDefault();
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        greetMsg = await invoke("greet", {name});
+    }
+</script>
+
+<main class="main-content">
+    <div class="sidebar">
+        <div class="sidebar-category" style="border-bottom: 5px #333333; margin: 0;">
+        Templates
+        </div>
+        <div>
+            <div class="sidebar-category">
+                Class 1
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+        </div>
+        <div>
+            <div class="sidebar-category">
+                Class 2
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+            <div class="sidebar-item">
+                <img src="/box.png" alt="the image to insert">
+                <p>Box</p>
+            </div>
+        </div>
+    </div>
+    <div class="editor">
+        Editor
+    </div>
+    <p>{greetMsg}</p>
+</main>
